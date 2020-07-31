441cf0e5a237617f4723821febc084ab2f502f3f
Fiona Zhou <fiona.zhou01@sap.com>
2020-07-31 14:22:29 +0800

    commit all

--- app/src/main/java/com/example/helloandroid/DisplayMessageActivity.java
+++ app/src/main/java/com/example/helloandroid/DisplayMessageActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import androidx.navigation.Navigation;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+
+    private static String message = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +19,30 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView2);
         textView.setText(message);
     }
+
+    public void goBack(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("editTextValue", "Welcome back " + message);
+        setResult(RESULT_OK, intent);
+        finish();
+
+//        // send message back to main page
+//        Bundle bundle = new Bundle();
+//        bundle.putString("message", "Welcome back " + message);
+//        Navigation.findNavController(view).navigate(R.id.editTextTextPersonName2, bundle);
+    }
+
+    public void goDraw(View view) {
+        Intent intent = new Intent(this, DrawActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        // get message from Display Message Activity
+//        EditText et = findViewById(R.id.editTextTextPersonName2);
+//        et.setText(getArguments().getString("message"));
+
     }
 
     /** Called when the user taps the Send button */
@@ -24,6 +30,17 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editTextTextPersonName2);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        startActivityForResult(intent, 1);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if(resultCode == RESULT_OK) {
+                String strEditText = data.getStringExtra("editTextValue");
+                EditText editText = (EditText) findViewById(R.id.editTextTextPersonName2);
+                editText.setText(strEditText);
+            }
+        }
     }
 }
